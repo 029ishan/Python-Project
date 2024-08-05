99d911289711c3ac9b56522a2c6c5de407a312ff
Ishan Khanal <ishankhanal242@gmail.com>
2024-08-05 17:26:17 +0545

    first push

---
+++ pythonProject/dataCollection.py
@@ -0,0 +1,56 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+import numpy as np
+import math
+
+cap = cv2.VideoCapture(0)
+detector = HandDetector(maxHands=1)
+
+offset = 20  # Offset for the bounding box
+imgSize = 300  # Size of the output image
+
+while True:
+    success, img = cap.read()
+    if not success:
+        continue  # Skip the frame if it was not read correctly
+
+    hands, img = detector.findHands(img)
+    if hands:
+        hand = hands[0]
+        x, y, w, h = hand['bbox']
+
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
+        y1 = max(0, y - offset)
+        y2 = min(img.shape[0], y + h + offset)
+        x1 = max(0, x - offset)
+        x2 = min(img.shape[1], x + w + offset)
+
+        imgCrop = img[y1:y2, x1:x2]
+        imgCropShape = imgCrop.shape
+
+        aspectRatio = h / w
+
+        if aspectRatio > 1:
+            k = imgSize / h
+            wCal = math.ceil(k * w)
+            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+            imgResizeShape = imgResize.shape
+            wGap = (imgSize - wCal) // 2
+            imgWhite[:, wGap:wGap + wCal] = imgResize
+        else:
+            k = imgSize / w
+            hCal = math.ceil(k * h)
+            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+            imgResizeShape = imgResize.shape
+            hGap = (imgSize - hCal) // 2
+            imgWhite[hGap:hGap + hCal, :] = imgResize
+
+        cv2.imshow("ImageCrop", imgCrop)
+        cv2.imshow("ImageWhite", imgWhite)
+
+    cv2.imshow("Image", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
